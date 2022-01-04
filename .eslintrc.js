b0a8da694757757b7a04a2286fdf502c6ce3e439
Jan Švanda <jan.svanda@avast.com>
2022-01-04 11:05:51 +0100

    husky added, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	parser: 'vue-eslint-parser',
+	extends: ['plugin:vue/recommended', 'eslint:recommended', "plugin:prettier/recommended"]
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	singleQuote: true,
+	printWidth: 80,
+	trailingComma: 'none',
+	tabWidth: 2,
+	semi: false,
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+	useTabs: true,
+}
